98f003fa51504d8a2fb1f3c7a14168f5628dbc05
Ashish Kha <ashish.kha@neosofttech.com>
2019-02-25 17:07:36 +0530

    runtime permission added for location

--- app/src/main/java/com/example/location/MainActivity.kt
+++ app/src/main/java/com/example/location/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.example.location
 
+import android.Manifest
 import android.content.Context
+import android.content.pm.PackageManager
 import android.location.Geocoder
 import android.location.Location
 import android.location.LocationListener
 import android.location.LocationManager
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
@@ -32,8 +36,37 @@ class MainActivity : AppCompatActivity(), LocationListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        requestLocationUsingLocationManager()
+        checkLocationPermission()
 
+    }
+
+    private fun checkLocationPermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+            != PackageManager.PERMISSION_GRANTED
+        ) {
+            grantPermission()
+        } else {
+            requestLocationUsingLocationManager()
+        }
+    }
+
+    private fun grantPermission() {
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+            MY_PERMISSIONS_REQUEST_READ_CONTACTS
+        )
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            MY_PERMISSIONS_REQUEST_READ_CONTACTS -> {
+                if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                    requestLocationUsingLocationManager()
+                }
+            }
+        }
     }
 
     private fun requestLocationUsingLocationManager() {
@@ -51,5 +84,6 @@ class MainActivity : AppCompatActivity(), LocationListener {
 
     companion object {
         private val TAG = MainActivity::class.qualifiedName
+        private val MY_PERMISSIONS_REQUEST_READ_CONTACTS = 100
     }
 }
